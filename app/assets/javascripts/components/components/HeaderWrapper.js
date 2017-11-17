2b253b95d201b89eed37d858544f0ca95e49c6a5
webguru103 <frankhou99@gmx.com>
2017-11-17 13:59:53 -0800

    Stash the components js files

--- app/assets/javascripts/components/components/HeaderWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  display: flex;
-  width: 100%;
-  height: 80px;
-  background: #00CDFF;
-  color: #FFFFFF;
-`;
--- app/assets/javascripts/components/components/LogoWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  display: flex;
-  flex: 1;
-  height: 80px;
-  padding: 10px 0px;
-  color: #FFFFFF;
-`;
