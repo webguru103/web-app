7e8d8fec8897ff89622f680f77912fd44533c02d
Igor <igor.polyakov@faceit-team.com>
2017-11-17 14:05:06 +0200

    Add more test gems;add user validation tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
+    first_name 'Fiona'
+    last_name 'Adler'
+    display_name 'Fiona'
+    timezone 'UTC'
     email 'fiona.adler@outmarket.com.au'
     password 'Sdfgs9834lk9E$T6DFAgljr'
     password_confirmation 'Sdfgs9834lk9E$T6DFAgljr'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { build(:user) }
+
+  before :each do
+    user.skip_confirmation!
+    user.save
+  end
 
   it 'is valid with valid attributes' do
     expect(user).to be_valid
@@ -10,10 +15,21 @@ RSpec.describe User, type: :model do
   it 'is invalid with blank attributes' do
     expect(User.create(email: '',
                        password: '',
-                       password_confirmation: '')).not_to be_valid
+                       password_confirmation: '',
+                       first_name: '',
+                       last_name: '',
+                       display_name: '',
+                       timezone: '')).not_to be_valid
   end
 
   describe 'User validations' do
-    it { expect(user).to validate_presence_of(:email) }
+    it { expect(user).to validate_presence_of(:first_name) }
+    it { expect(user).to validate_presence_of(:last_name) }
+    it { expect(user).to validate_presence_of(:display_name) }
+    it { expect(user).to validate_presence_of(:timezone) }
+    it { expect(user).to validate_length_of(:email).is_at_most(100) }
+    it { expect(user).to validate_length_of(:first_name).is_at_most(50) }
+    it { expect(user).to validate_length_of(:last_name).is_at_most(50) }
+    it { expect(user).to validate_length_of(:display_name).is_at_most(50) }
   end
 end
